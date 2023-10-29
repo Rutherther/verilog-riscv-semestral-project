30a7f9492e5e15f2d64dded11bc5080af6b54ec5
Rutherther <rutherther@proton.me>
2023-10-29 08:59:09 +0100

    feat: add basic testing programs

---
+++ programs/add.c
@@ -0,0 +1,11 @@
+int add(int a, int b)
+{
+    return a + b;
+}
+
+void main()
+{
+    int a = 20;
+    int b = 30;
+    int c = add(a, b);
+}
---
+++ programs/tests.c
@@ -0,0 +1,7 @@
+void main()
+{
+	signed char a = -10;
+	signed char b = a << 1;
+	unsigned char c = ((unsigned char)b) >> 1;
+	signed char d = b >> 1;
+}
