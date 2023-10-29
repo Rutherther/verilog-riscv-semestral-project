e7b5d989532b0690f2b0ef3a1b7a0072903c0d51
Rutherther <rutherther@proton.me>
2023-10-29 19:35:42 +0100

    test: add cpu testbenches for c programs

---
+++ testbench/tb_cpu_branches.sv
@@ -0,0 +1,57 @@
+import cpu_types::*;
+
+module tb_cpu_branches();
+  reg clk, rst_n;
+
+  wire [31:0] memory_address, memory_write, memory_out;
+  wire        memory_we;
+
+  memory_mask_t memory_mask;
+
+  wire [31:0] pc;
+  reg [31:0]  instruction;
+
+  cpu uut(
+    .clk(clk),
+    .rst_n(rst_n),
+
+    .instruction(instruction),
+    .pc(pc),
+
+    .memory_address(memory_address),
+    .memory_out(memory_out),
+    .memory_write(memory_write),
+    .memory_mask(memory_mask),
+    .memory_we(memory_we)
+  );
+
+  ram memory_inst(
+    .clk(clk),
+    .a(memory_address),
+    .mask(memory_mask),
+    .we(memory_we),
+    .wd(memory_write),
+    .rd(memory_out)
+  );
+
+  file_program_memory #(.FILE_NAME("programs/bin/branches.dat")) prog_mem_inst(
+    .addr(pc[11:0]),
+    .instruction(instruction)
+  );
+
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+  end
+
+  initial begin
+    $dumpfile("waves/tb_cpu_branches.vcd");
+    $dumpvars;
+
+    rst_n = 0;
+    #20
+    rst_n = 1;
+
+    #500 $finish;
+  end
+endmodule
