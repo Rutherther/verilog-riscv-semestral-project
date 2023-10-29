adfdc041e204e13c59c32d866fb2ee288b272c57
Rutherther <rutherther@proton.me>
2023-10-29 10:04:09 +0100

    feat: add branches.c test

---
+++ programs/branches.c
@@ -0,0 +1,43 @@
+void main()
+{
+    int a = 1;
+    int b = 5;
+    int c = 0;
+
+    if (a < b) {
+        c = 1;
+    } else {
+        c = 2;
+    }
+
+    if (a >= b) {
+        c = 1;
+    } else {
+        c = 2;
+    }
+
+    if (a != b) {
+        c = 1;
+    } else {
+        c = 2;
+    }
+
+    if (a == b) {
+        c = 1;
+    } else {
+        c = 2;
+    }
+
+    if (a <= b) {
+        c = 1;
+    } else {
+        c = 2;
+    }
+
+    if (a > b) {
+        c = 1;
+    } else {
+        c = 2;
+    }
+
+}
